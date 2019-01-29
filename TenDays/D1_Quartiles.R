f978c10b14d57599dfdf50c80a029237162af199
Anirudh Mylavarapu <anirudhhimself@gmail.com>
2019-01-28 22:38:51 -0600

    Comments

--- TenDays/D1_Quartiles.R
+++ TenDays/D1_Quartiles.R
@@ -2,13 +2,12 @@ rm(list=ls())
 
 raw_input <- read.table("~/Desktop/input", fill=TRUE)
 
-
 data <- raw_input[2,]
 sorted.data <- sort(data)
 
 # HackerRank has a weird definition of quartiles that 
 # doesn't include the median in the calculation of the 
-# 25% and 75% values so we have to calculate it ourselves
+# 25% and 75% values so we can't use the built in quantiles function.
 median <- median(as.numeric(sorted.data))
 
 lower.half <- sorted.data[sorted.data < median]
@@ -18,6 +17,6 @@ Q1 <- median(lower.half)
 Q3 <- median(upper.half)
 
 
-cat(q1)
-cat(median)
-cat(q2)
+cat(Q1, "\n")
+cat(median, "\n")
+cat(Q3, "\n")
