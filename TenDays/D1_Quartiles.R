ebf62af8e65c701fc4766ceac2dbc3f42be0a39b
Anirudh Mylavarapu <anirudhhimself@gmail.com>
2019-01-28 22:37:09 -0600

    Completed Quartiles Challenge

---
+++ TenDays/D1_Quartiles.R
@@ -0,0 +1,23 @@
+rm(list=ls())
+
+raw_input <- read.table("~/Desktop/input", fill=TRUE)
+
+
+data <- raw_input[2,]
+sorted.data <- sort(data)
+
+# HackerRank has a weird definition of quartiles that 
+# doesn't include the median in the calculation of the 
+# 25% and 75% values so we have to calculate it ourselves
+median <- median(as.numeric(sorted.data))
+
+lower.half <- sorted.data[sorted.data < median]
+upper.half <- sorted.data[sorted.data > median]
+
+Q1 <- median(lower.half)
+Q3 <- median(upper.half)
+
+
+cat(q1)
+cat(median)
+cat(q2)
